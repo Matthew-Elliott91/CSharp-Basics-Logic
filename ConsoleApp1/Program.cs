f977ec37ccf3307f7a205f5885a3b6d99ae32478
Matthew Elliott <m.elliott091091@gmail.com>
2024-12-24 14:52:50 +0000

    More C# practice

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,4 +13,43 @@ if (!isRainy == false) {
 // ||
 // Not !
 
+// Relational Operators 
+
+int num1 = 0;
+int num2 = 0;
+
+bool isEqual = num1 == num2;
+/*
+Console.WriteLine("Please guess a number");
+
+if (num1 == int.Parse(Console.ReadLine()))
+{
+    Console.WriteLine("Your guess was correct");
+}
+else {
+    Console.WriteLine("Your guess was incorrect");
+}*/
+
+bool isWithParents = false;
+Console.WriteLine("Please enter your age");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Are you with your parents");
+string parentsQuestion = Console.ReadLine();    
+
+
+
+
+if (age >= 18 && isWithParents)
+{
+    Console.WriteLine("You can go to the club");
+}
+else if (age <= 10)
+{
+    Console.WriteLine("Go to Primary School");
+}
+else 
+{ 
+    Console.WriteLine("Go to Secondary School");
+}
+
 Console.ReadKey();
