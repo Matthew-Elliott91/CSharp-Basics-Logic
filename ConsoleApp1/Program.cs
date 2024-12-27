005f8596334e6b1343819d972010a9bd2b69e5f4
Matthew Elliott <m.elliott091091@gmail.com>
2024-12-27 11:16:01 +0000

    Practiced more if statements and switch statements as well as one line if statements

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // Bool is a datatype in C#
+using System.Net.Sockets;
+
 bool isRainy = false;
 bool hasUmbrella = true;
 
@@ -15,10 +17,32 @@ if (!isRainy == false) {
 
 // Relational Operators 
 
-int num1 = 0;
+/* int num1 = 0;
 int num2 = 0;
+int age = 0;
+string address;
 
+Console.WriteLine("How old are you");
+age = int.Parse(Console.ReadLine());
+
+if (age >= 18)
+{
+    Console.WriteLine("You can go to the club");
+}
+else
+{
+    Console.WriteLine("Are you with your parents");
+    string withParents = Console.ReadLine();
+    if (withParents == "yes")
+    {
+        Console.WriteLine("You can go to the club");
+    }
+    else { Console.WriteLine("You can not go to the club"); }
+}
 bool isEqual = num1 == num2;
+bool isNotEqual = num1 != num2;
+*/
+// Console.WriteLine("Please enter a whole number");
 /*
 Console.WriteLine("Please guess a number");
 
@@ -30,26 +54,83 @@ else {
     Console.WriteLine("Your guess was incorrect");
 }*/
 
-bool isWithParents = false;
-Console.WriteLine("Please enter your age");
-int age = int.Parse(Console.ReadLine());
-Console.WriteLine("Are you with your parents");
-string parentsQuestion = Console.ReadLine();    
+//bool isWithParents = false;
+//Console.WriteLine("Please enter your age");
+//int age = int.Parse(Console.ReadLine());
+//Console.WriteLine("Are you with your parents");
+//string parentsQuestion = Console.ReadLine();    
 
 
 
 
-if (age >= 18 && isWithParents)
+/*if (num1 == int.Parse(Console.ReadLine()))
 {
-    Console.WriteLine("You can go to the club");
+    Console.WriteLine("The numbers are equal");
+
+    Console.WriteLine("Please enter your age");
+    age = int.Parse(Console.ReadLine());  
+    if (age >= 18)
+    {
+        Console.WriteLine("Enter your address so we can send you a prize");
+        address = Console.ReadLine();    
+    } else
+    {
+        Console.WriteLine("You are not old enough to win a prize");
+    }
 }
-else if (age <= 10)
+else
 {
-    Console.WriteLine("Go to Primary School");
+    Console.WriteLine("Numbers are not equal");
+}*/
+
+// One line if statements or shorthand
+int month = 5;
+string monthName;
+
+if (month == 1)
+{ monthName = "January";
+} else if (month == 2) {
+    monthName = "February";
+} else if (month == 3)
+{
+    monthName = "March";
+} else if (month == 4)
+{
+    monthName = "Unknown";
 }
-else 
-{ 
-    Console.WriteLine("Go to Secondary School");
+
+    // Switch Statement
+
+    switch (month)
+{
+    case 1: monthName = "January";
+        break;
+    case 2: monthName = "February";
+        break;
+    case 3: monthName = "March";
+        break;  
+
+    default:
+        monthName = "Unknown";
+        break;
+}
+
+int day = 3;
+
+switch (day)
+{
+    case 1: day = 1;
+        Console.WriteLine("Monday");
+        break;
+    case 2: day = 2;
+        Console.WriteLine("Tuesday");
+        break;
+    case 3: day = 3;
+        Console.WriteLine("Wednesday");
+        break;
+    default:
+        Console.WriteLine("Anotherday");
+        break;
 }
 
 Console.ReadKey();
